a36bdfc019c701384efab26977e04221d77333b3
MayChehab <mchehab@proximie.com>
2024-11-13 12:17:29 +0200

    chore: rename tool to result-analyzer

--- README.md
+++ README.md
@@ -1,21 +1,28 @@
-# XCResult Analyzer
+#XCResult Analyzer
 
-A tool designed to process and analyze .xcresult bundles generated from Xcode test runs. It generates sqlite3 database and outputs a detailed report summarizing test results such as test summary and failure details.
+A tool designed to process and analyze
+            .xcresult bundles generated from Xcode test runs
+            .It generates sqlite3 database and outputs a detailed report
+                summarizing test results such as test summary and failure
+                    details
+            .
 
-## Example Usage
+        ##Example Usage
 
-```shell
-xcra generate report --path ./output-report.xcresult --template markdown -o result.md
+```shell xra generate report-- path./
+        output -
+    report.xcresult-- template markdown -
+    o result.md
 ```
 
-```shell
-xcra generate failure-report --path ./output-report.xcresult --template slack -o result.json
+```shell xra generate failure - report-- path./ output -
+    report.xcresult-- template slack -
+    o result.json
 ```
 
-```shell
-xcra generate database --path ./output-report.xcresult
+```shell xra generate database-- path./ output -
+    report.xcresult
 ```
 
-```shell
-xcra get summary --path ./output-report.xcresult
+```shell xra get summary-- path./ output - report.xcresult
 ```
--- xra/src/main.rs
+++ xra/src/main.rs
@@ -13,14 +13,14 @@ fn main() -> anyhow::Result<()> {
     let args = Cli::try_parse()?;
     match args {
         Cli::Generate(Generate::Database { xcresult_path }) => {
-            xcra_core::generate_xcresult_db(&xcresult_path)?;
+            xra_core::generate_xcresult_db(&xcresult_path)?;
         }
         Cli::Generate(Generate::FailureReport {
             xcresult_path,
             template,
             output,
         }) => {
-            let params = xcra_core::generate_failure_report(&xcresult_path)?;
+            let params = xra_core::generate_failure_report(&xcresult_path)?;
             let reg = Handlebars::new();
             let content = template.template();
             let content = reg.render_template(&content, &params)?;
@@ -31,7 +31,7 @@ fn main() -> anyhow::Result<()> {
             template,
             output,
         }) => {
-            let params = xcra_core::generate_tests_report(&xcresult_path)?;
+            let params = xra_core::generate_tests_report(&xcresult_path)?;
             let mut reg = Handlebars::new();
             reg.register_helper("toEmoji", Box::new(hbs_helpers::result_to_emoji));
             let content = template.template();
@@ -39,13 +39,13 @@ fn main() -> anyhow::Result<()> {
             fs::write(output, content)?;
         }
         Cli::Get(Get::Summary { xcresult_path }) => {
-            xcra_core::get_summary(&xcresult_path)?;
+            xra_core::get_summary(&xcresult_path)?;
         }
         Cli::Get(Get::FailureReport { xcresult_path }) => {
-            xcra_core::get_failure_report(&xcresult_path)?;
+            xra_core::get_failure_report(&xcresult_path)?;
         }
         Cli::Get(Get::Report { xcresult_path }) => {
-            xcra_core::get_tests_report(&xcresult_path)?;
+            xra_core::get_tests_report(&xcresult_path)?;
         }
     }
     Ok(())
